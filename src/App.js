d05e96b518a78435db10f42667af998c828107c5
Serge Lerner <serge@yodas.com>
2016-10-04 13:07:29 +0300

    Move raven init to index.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import logo from './logo.svg';
 import './App.css';
 
 
-Raven
-  .config('https://819e0d4d48d14194ac38a29ca72488e1@sentry.io/102888')
-  .install();
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Raven from 'raven-js';
 import App from './App';
 import './index.css';
+
+Raven
+  .config('https://819e0d4d48d14194ac38a29ca72488e1@sentry.io/102888')
+  .install();
 
 ReactDOM.render(
   <App />,
