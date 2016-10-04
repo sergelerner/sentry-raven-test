f2d689f38eeb54f29d48e1ced2cbc0c9debe8946
Serge Lerner <serge@yodas.com>
2016-10-04 13:28:04 +0300

    Testing captureException by throwing an Error object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import Raven from 'raven-js';
 import logo from './logo.svg';
 import './App.css';
-
-
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import Raven from 'raven-js';
 import App from './App';
 import './index.css';
+import _ from 'lodash';
 
 Raven
   .config('https://819e0d4d48d14194ac38a29ca72488e1@sentry.io/102888')
   .install();
+
+try {
+  const dude = {};
+  const isSinger = _.has(dude, ['talent', 'sing']);
+
+  if (!isSinger) {
+    throw new Error('Dude is not a singer');
+  }
+
+} catch(e) {
+  Raven.captureException(e);
+}
 
 ReactDOM.render(
   <App />,
